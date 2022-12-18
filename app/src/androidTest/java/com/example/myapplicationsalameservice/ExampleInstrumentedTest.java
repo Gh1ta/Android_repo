fb8e15e53038b118297be1e54159e132b23c0256
Gh1ta <salame.ghita@inemail.ine.inpt.ma>
2022-12-18 18:23:27 +0100

    Concept of activity

--- app/src/androidTest/java/com/example/myapplicationsalameservice/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplicationsalameservice/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication_ghitasalame;
+package com.example.myapplicationsalameservice;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication_ghitasalame", appContext.getPackageName());
+        assertEquals("com.example.myapplicationsalameservice", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/myapplicationsalameservice/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplicationsalameservice/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication_ghitasalame;
+package com.example.myapplicationsalameservice;
 
 import org.junit.Test;
 
