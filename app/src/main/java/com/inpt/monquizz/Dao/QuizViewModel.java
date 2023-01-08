1c04c5e2c1029ed4d3575d5d14814d2ebe7e4bd8
Gh1ta <salame.ghita@inemail.ine.inpt.ma>
2023-01-08 23:11:45 +0100

    Quiz App

---
+++ app/src/main/java/com/inpt/monquizz/Dao/QuizViewModel.java
@@ -0,0 +1,32 @@
+package com.inpt.monquizz.Dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.inpt.monquizz.Model.QuizModel;
+
+import java.util.List;
+
+public class QuizViewModel extends ViewModel implements FirebaseRepository.OnFireStoreDataAdded {
+    MutableLiveData<List<QuizModel>> quizModelListData = new MutableLiveData<>();
+    FirebaseRepository firebaseRepo = new FirebaseRepository(this);
+
+    public QuizViewModel(){
+        firebaseRepo.getDataFromFireStore();
+    }
+
+    public LiveData<List<QuizModel>> getLiveDatafromFireStore() {
+        return  quizModelListData;
+    }
+
+    @Override
+    public void quizDataAdded(List<QuizModel> quizModelList) {
+        quizModelListData.setValue(quizModelList);
+    }
+
+    @Override
+    public void OnError(Exception e) {
+    }
+}
+
